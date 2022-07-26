ba6e129a8a2eeb5cd76e93d7e24fef77abea96d0
Matheus-Christian <86934913+Matheus-Christian@users.noreply.github.com>
2022-07-25 20:23:47 -0400

    first commit

---
+++ src/main/java/br/com/matheus/linguagensapi/Linguagem.java
@@ -0,0 +1,35 @@
+package br.com.matheus.linguagensapi;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "pricipaisLinguagens")
+public class Linguagem {
+	
+	@Id
+	private String id;
+	//Atributos
+	private String title;
+	private String image;
+	private int ranking;
+	
+	//Um construtor serve para instanciar objetos da classe onde foi definido
+	public Linguagem(String title, String image, int ranking) {
+		this.title = title;
+		this.image = image;
+		this.ranking = ranking;
+	}
+	
+	//método getter retorna seu valor
+	public String getTitle() {
+		return title;
+	}
+	public String getImage() {
+		return image;
+	}
+	public int getRanking() {
+		return ranking;
+	}
+	
+	
+}
---
+++ src/main/java/br/com/matheus/linguagensapi/LinguagemController.java
@@ -0,0 +1,22 @@
+package br.com.matheus.linguagensapi;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController //controla as requisicoes indicando quem deve receber e para quem deve responde-las
+public class LinguagemController {
+	
+	@Autowired //fornece controle sobre onde e como a ligacao entre os beans deve ser realizada
+	private LinguagemRepository repositorio;
+	
+	@GetMapping(value="/linguagens")
+	public List<Linguagem> obterLinguagens() {
+		List<Linguagem> linguagens = repositorio.findAll();
+		return linguagens;
+	}
+}
